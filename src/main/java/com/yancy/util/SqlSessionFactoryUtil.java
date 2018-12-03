71a9b6c27dd91f72e92d93a8a33775f4a048c682
Yancy <Yancy@DESKTOP-AHUOE0F>
2018-12-03 20:34:35 +0800

    格式化代码

--- src/main/java/com/yancy/util/SqlSessionFactoryUtil.java
+++ src/main/java/com/yancy/util/SqlSessionFactoryUtil.java
@@ -2,7 +2,7 @@ package com.yancy.util;
 
 import java.io.IOException;
 import java.io.InputStream;
- 
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,36 +12,39 @@ import org.apache.log4j.Priority;
 
 /**
  * mybatis工具类
+ * 
  * @author yancy
  *
  */
 public class SqlSessionFactoryUtil {
 	private static SqlSessionFactory sqlSessionFactory = null;
-	private static final Class CLASS_LOCK=SqlSessionFactoryUtil.class;
-	
+	private static final Class CLASS_LOCK = SqlSessionFactoryUtil.class;
+
 	private SqlSessionFactoryUtil() {
-		//私有化构造方法
+		// 私有化构造方法
 	}
+
 	public static SqlSessionFactory initSqlSessionFactory() {
 		String resource = "mybatis-config.xml";
 		InputStream inputStream = null;
 		try {
-			inputStream=Resources.getResourceAsStream(resource);
+			inputStream = Resources.getResourceAsStream(resource);
 		} catch (IOException e) {
 			Logger.getLogger(SqlSessionFactoryUtil.class.getName()).log(Priority.DEBUG, null, e);
 		}
 		synchronized (CLASS_LOCK) {
-			if(sqlSessionFactory==null) {
-				sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+			if (sqlSessionFactory == null) {
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 			}
 		}
 		return sqlSessionFactory;
 	}
+
 	public static SqlSession openSqlSession() {
-		if(sqlSessionFactory==null) {
-			 initSqlSessionFactory();
+		if (sqlSessionFactory == null) {
+			initSqlSessionFactory();
 		}
 		return sqlSessionFactory.openSession();
 	}
-	
+
 }
